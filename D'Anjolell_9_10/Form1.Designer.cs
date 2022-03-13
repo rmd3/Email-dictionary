37621529b8fa412e4a858ba8eaa3ce7e3cd0a711
rmd3 <michaeldanjolell@gmail.com>
2022-03-13 18:00:30 -0400

    Add project files.

---
+++ D'Anjolell_9_10/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace D_Anjolell_9_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnLkUp = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblTitle.Location = new System.Drawing.Point(211, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(403, 39);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Name and Email Address";
+            // 
+            // btnChange
+            // 
+            this.btnChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnChange.Location = new System.Drawing.Point(262, 269);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(131, 51);
+            this.btnChange.TabIndex = 1;
+            this.btnChange.Text = "Change";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // btnLkUp
+            // 
+            this.btnLkUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnLkUp.Location = new System.Drawing.Point(262, 164);
+            this.btnLkUp.Name = "btnLkUp";
+            this.btnLkUp.Size = new System.Drawing.Size(131, 51);
+            this.btnLkUp.TabIndex = 2;
+            this.btnLkUp.Text = "Lookup";
+            this.btnLkUp.UseVisualStyleBackColor = true;
+            this.btnLkUp.Click += new System.EventHandler(this.btnLkUp_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnCreate.Location = new System.Drawing.Point(450, 164);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(131, 51);
+            this.btnCreate.TabIndex = 3;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnDelete.Location = new System.Drawing.Point(450, 269);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(131, 51);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnExit.Location = new System.Drawing.Point(355, 363);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(131, 51);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtName.Location = new System.Drawing.Point(431, 87);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(183, 38);
+            this.txtName.TabIndex = 6;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lblName.Location = new System.Drawing.Point(238, 90);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(166, 31);
+            this.lblName.TabIndex = 7;
+            this.lblName.Text = "Enter Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnLkUp);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnLkUp;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ D'Anjolell_9_10/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using Microsoft.VisualBasic;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D_Anjolell_9_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<string, string> emailDictionary = new Dictionary<string, string>();
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Closes the program
+            this.Close();
+        }
+
+        private void btnLkUp_Click(object sender, EventArgs e)
+        {
+            //Richard D'Anjolell
+            //Try catch statement if there is an error accessing the file
+            try
+            {
+                //Declare Variables
+                string emailerName;
+                string fileLine;
+                StreamReader emailFile;
+
+                //Open dictionary file
+                emailFile = File.OpenText("..\\..\\emailDictionary.txt");
+
+                int indexNum = 0;
+                while (!emailFile.EndOfStream) 
+                {
+                    fileLine = emailFile.ReadLine();
+                    string[] fileSplit = fileLine.Split(' ');
+                    emailDictionary.Add(fileSplit[indexNum], fileSplit[indexNum+1]);
+                    indexNum += 2;
+                }
+
+                emailerName = txtName.Text;
+
+                if (emailDictionary.ContainsKey(emailerName))
+                {
+                    string outOfStock = emailerName + "'s email is " + emailDictionary[emailerName];
+                    string windowTitle = "Email Lookup";
+                    MessageBox.Show(outOfStock, windowTitle);
+                }
+
+                else 
+                {
+                    string outOfStock = emailerName + " has no entry in this dictionary.";
+                    string windowTitle = "Email Lookup";
+                    MessageBox.Show(outOfStock, windowTitle);
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string emailerName = txtName.Text;
+            string newEmail;
+            newEmail = Interaction.InputBox("Enter the email address:", "Email Creator", "");
+            emailDictionary.Add(emailerName, newEmail);
+            string outOfStock = emailerName + " and their email has been added to the dictionary.";
+            string windowTitle = "Email Created";
+            MessageBox.Show(outOfStock, windowTitle);
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            //Try catch statement if there is an error accessing the file
+            try
+            {
+                //Declare Variables
+                string emailerName;
+                string changedEmail;
+                string fileLine;
+                StreamReader emailFile;
+
+                //Open dictionary file
+                emailFile = File.OpenText("..\\..\\emailDictionary.txt");
+
+                int indexNum = 0;
+                while (!emailFile.EndOfStream)
+                {
+                    fileLine = emailFile.ReadLine();
+                    string[] fileSplit = fileLine.Split(' ');
+                    emailDictionary.Add(fileSplit[indexNum], fileSplit[indexNum + 1]);
+                    indexNum += 2;
+                }
+
+                emailerName = txtName.Text;
+
+                if (emailDictionary.ContainsKey(emailerName))
+                {
+                    changedEmail = Interaction.InputBox("Enter their new email address:", "Email Changer", "");
+                    emailDictionary[emailerName] = changedEmail;
+                    string outOfStock = emailerName + "'s email has been changed.";
+                    string windowTitle = "Email Changer";
+                    MessageBox.Show(outOfStock, windowTitle);
+                }
+
+                else
+                {
+                    string outOfStock = emailerName + " has no entry in this dictionary.";
+                    string windowTitle = "Email Changer";
+                    MessageBox.Show(outOfStock, windowTitle);
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //Try catch statement if there is an error accessing the file
+            try
+            {
+                //Declare Variables
+                string emailerName;
+                string fileLine;
+                StreamReader emailFile;
+
+                //Open dictionary file
+                emailFile = File.OpenText("..\\..\\emailDictionary.txt");
+
+                int indexNum = 0;
+                while (!emailFile.EndOfStream)
+                {
+                    fileLine = emailFile.ReadLine();
+                    string[] fileSplit = fileLine.Split(' ');
+                    emailDictionary.Add(fileSplit[indexNum], fileSplit[indexNum + 1]);
+                    indexNum += 2;
+                }
+
+                emailerName = txtName.Text;
+
+                if (emailDictionary.ContainsKey(emailerName))
+                {
+                    emailDictionary.Remove(emailerName);
+                    string outOfStock = emailerName + " and their email has been deleted from the dictionary.";
+                    string windowTitle = "Email Deleter";
+                    MessageBox.Show(outOfStock, windowTitle);
+                }
+
+                else
+                {
+                    string outOfStock = emailerName + " has no entry in this dictionary.";
+                    string windowTitle = "Email Deleter";
+                    MessageBox.Show(outOfStock, windowTitle);
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
